cb292c9664fea1b1d1cc5bf2c5e9ee009bfc94f1
gladysluke <lukegladys615@gmail.com>
2023-11-30 16:51:23 -0800

    Third Commit

--- signupValidationSchema.js
+++ signupValidationSchema.js
@@ -1,9 +1,9 @@
 import * as Yup from 'yup';
 
 const signupValidationSchema = Yup.object().shape({
-    name: yup.string().required("Required"),
-    email: yup.string().email("Invalid email").required("Required"),
-    password: yup.string().required("Required"),
+    name: Yup.string().required("Required"),
+    email: Yup.string().email("Invalid email").required("Required"),
+    password: Yup.string().required("Required"),
 });
 
 export default signupValidationSchema;
